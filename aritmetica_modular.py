d1f37d6570517385df2765264eb22e30a5561518
arturbmallmann <arturbmallmann@gmail.com>
2021-03-24 08:48:35 -0300

    first commit

---
+++ aritmetica_modular.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+multiply_ar = lambda vec,n:[v * n for v in vec]
+def tabuada_mod(num, mod):
+	mult = [multiply_ar([x for x in range(num)],x) for x in range(num)]
+	print('\n')
+	for x in mult:
+		linha =''
+		for y in x:
+				linha += f'{y%mod:3}'
+		print (linha)
+
+tabuada_mod(10,100)
+tabuada_mod(10,10)
+tabuada_mod(7,7)
+tabuada_mod(6,6)
+tabuada_mod(5,5)
